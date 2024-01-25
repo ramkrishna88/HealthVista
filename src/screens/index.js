51950dffd0d70c5f75ed41ae1f23654fcf3c2c56
ramkrishna88 <chowdaryrk077@gmail.com>
2024-01-25 22:40:10 +0000

    SplashScreen

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Appointment from './appointment';
+import Chat from './chat';
+import Home from './home';
+import Login from './login';
+import Profile from './profile';
+import Register from './register';
+import Splash from './splash';
+
+export {Appointment, Chat, Home, Login, Profile, Register, Splash};
